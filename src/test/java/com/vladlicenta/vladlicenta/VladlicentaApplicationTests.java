3eec217a8188794cc183f26afa81767531e1c21a
ghiurtuv <ghiurtu_vlad@yahoo.com>
2018-05-13 16:00:32 +0300

    Recreated project

--- src/test/java/com/vladlicenta/vladlicenta/VladlicentaApplicationTests.java
+++ src/test/java/com/vladlicenta/vladlicenta/VladlicentaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.VladBoot;
+package com.vladlicenta.vladlicenta;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTests {
+public class VladlicentaApplicationTests {
 
 	@Test
 	public void contextLoads() {
